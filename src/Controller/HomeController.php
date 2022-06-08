d966d30bca6f60578f7d54fc5551cceb11ff1ffe
Mehdi KACIM <Mehdi.KACIM@guelt.com>
2022-06-08 11:48:01 +0200

    Login form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,16 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_start')]
+    #[Route('/', name: 'app')]
     public function index(): Response
     {
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'user_theme_key' => 'business'
+        ]);
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
